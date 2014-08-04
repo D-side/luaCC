887dc3d32ac2918ae055cbb617310f0efc74a188
Paul Peganov <D-side@users.noreply.github.com>
2014-08-04 11:57:40 +0400

    A little `fs` wrapper

---
+++ file.lua
@@ -0,0 +1,21 @@
+File = {}
+
+-- Merely a ghost of an existing file!
+function File:new(fullpath)
+  local file = {path = fullpath}
+  self.__index = self
+  return setmetatable(file, self)
+end
+
+-- Delete - if exists
+function File:delete()
+  -- Does it even exist?
+  if ~(fs.exists(self.path))
+    return false, "No such file or directory"
+  -- ...and is it writable?
+  if fs.isReadOnly(self.path)
+    return false, "Access denied"
+  -- Alright, do it.
+  fs.delete(self.path)
+  return true, "Operation successful"
+end
