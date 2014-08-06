526665b50a0563adfe84378b2016c8a41d2b8223
D-side <side.spb@gmail.com>
2014-08-06 09:29:11 +0400

    Spaced indentation

--- iterate.lua
+++ iterate.lua
@@ -5,13 +5,13 @@ function times(n, f)
 end
 
 function range(a, b, f)
-	for i = a, b do
-		f(i)
-	end
+  for i = a, b do
+    f(i)
+  end
 end
 
 function map(t, f)
-	for k, v in pairs(t) do
-		t[k] = f(v)
-	end
+  for k, v in pairs(t) do
+    t[k] = f(v)
+  end
 end
